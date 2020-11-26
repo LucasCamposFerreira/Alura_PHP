4877f1240b8565f65a46d71db28927a6eb181911
LucasCamposFerreira <20lucas.campos05@gmail.com>
2020-11-26 17:23:13 -0300

    commit on master

---
+++ foreach.php
@@ -0,0 +1,21 @@
+<?php 
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'João',
+        'saldo' => 1000.00
+    ],
+    '123.456.789-11' => [
+        'titular' => 'Pedro',
+        'saldo' => 10.00
+    ],
+    '123.456.789-12' => [
+        'titular' => 'Maria',
+        'saldo' => 100.00
+    ]
+];
+
+foreach ($contasCorrentes as $cpf => $contas) {
+    echo $cpf . "</br>";
+
+}
