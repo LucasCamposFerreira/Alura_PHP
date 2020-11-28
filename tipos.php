438fe0e25d3062beea8cbc8ee5957ebef9e29a18
LucasCamposFerreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2020-11-28 11:40:22 -0300

    Add files via upload

---
+++ tipos.php
@@ -0,0 +1,13 @@
+<?php 
+
+$idade = 14;
+
+$salario = 1100.50;
+echo "valor: $salario/" ; 
+echo gettype($salario);
+
+echo '<br />';
+
+$salario = 1;
+echo "valor: $salario/" ; 
+echo gettype($salario);
