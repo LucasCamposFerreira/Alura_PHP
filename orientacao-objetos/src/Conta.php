2c9061ff5ea6415742d7cf86875bde5c83881c65
LucasCamposFerreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2021-01-21 10:32:04 -0300

    Update Conta.php

--- orientacao-objetos/src/Conta.php
+++ orientacao-objetos/src/Conta.php
@@ -1,22 +1,37 @@
 <?php
 
 class Conta
-{
-    public $cpfTitular;
-    public $nomeTitular;
-    public $saldo = 0;
 
-    public function sacar(float $valorASacar): void
+{
+    private $titular;
+    private $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(Titular $titular) 
+    {
+        $this->titular = $titular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;//Self vai se referir à classe em que está sendo digitado o código
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
             return;
+            
         }
 
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
         if ($valorADepositar < 0) {
             echo "Valor precisa ser positivo";
@@ -26,7 +41,7 @@ class Conta
         $this->saldo += $valorADepositar;
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
         if ($valorATransferir > $this->saldo) {
             echo "Saldo indisponível";
@@ -35,5 +50,25 @@ class Conta
 
         $this->sacar($valorATransferir);
         $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->titular->recuperaNome();
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->titular->recuperaCpf();
+    }
+
+    public static function recuperaNumDeContas() : int
+    {
+        return Conta::$numeroDeContas;
     }
 }
