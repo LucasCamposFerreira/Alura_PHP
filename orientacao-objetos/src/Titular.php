aff993fde7aa534fb098dc4db5137c35fe927a82
LucasCamposFerreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2021-01-21 10:31:55 -0300

    Create Titular.php

---
+++ orientacao-objetos/src/Titular.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Titular
+{
+    private $cpf;
+    private $nome;
+
+    public function __construct(string $cpf, string $nome)
+    {
+        $this->cpf = $cpf;
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa de ter 5 ou mais caracteres";
+            exit;
+        }
+    }
+
+    public function recuperaCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
+}
+
