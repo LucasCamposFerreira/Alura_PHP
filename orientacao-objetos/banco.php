945b31e13e0885cd284c0d21f1e45dd9d55de644
LucasCamposFerreira <66262807+LucasCamposFerreira@users.noreply.github.com>
2021-02-20 13:00:00 -0300

    Update banco.php

--- orientacao-objetos/banco.php
+++ orientacao-objetos/banco.php
@@ -2,17 +2,22 @@
 
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
+require_once 'src/CPF.php';
 
-$primeiraConta = new Conta(new Titular('123.456.789-10', 'Lucas Campos'));
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
-echo $primeiraConta->recuperaNomeTitular() . "</br>";
-echo $primeiraConta->recuperaSaldo() . "</br>";
-echo $primeiraConta->recuperaCpfTitular() . "</br>";
+echo $primeiraConta->recuperaNomeTitular() .'<br>';
+echo $primeiraConta->recuperaCpfTitular() .'<br>';
+echo $primeiraConta->recuperaSaldo() .'<br>';
 
-$segundaConta = new Conta(new Titular('123.456.789-11', 'Bruno Caldas'));
-echo"<pre>";
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$segundaConta = new Conta($patricia);
+echo'<prev>';
 print_r($segundaConta);
 
-echo Conta::recuperaNumDeContas();
+$outra = new Conta(new Titular(new CPF('123.456.789-13'), 'Abcdefg'));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();       
